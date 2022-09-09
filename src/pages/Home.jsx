716a7d90a7b1372562d1a0a04df0e715486de771
Douglas Monteiro <douglasm.dev@gmail.com>
2022-09-09 12:37:09 -0300

    finish trendind seller component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import HeroSection from "../components/ui/HeroSection";
 import LiveAuction from "../components/ui/Live-auction/LiveAuction";
+import SellerSection from "../components/ui/Seller-section/SellerSection";
+import TrendingSection from "../components/ui/Trending-section/TrendingSection";
 
 const Home = () => {
   return (
     <>
       <HeroSection />
       <LiveAuction />
+      <SellerSection />
+      <TrendingSection />
     </>
   );
 };
