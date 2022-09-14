15322f9fc53b7e9696c91f410a45d89676b9990c
Douglas Monteiro <douglasm.dev@gmail.com>
2022-09-14 18:24:33 -0300

    finish project

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,81 @@
-import React from "react";
+import React, { useRef } from "react";
+import { Container, Row, Col } from "reactstrap";
+
+import CommonSection from "../components/ui/Common-section/CommonSection";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  const nameRef = useRef("");
+  const emailRef = useRef("");
+  const subjectRef = useRef("");
+  const messageRef = useRef("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <CommonSection title='Contact' />;
+      <section>
+        <Container>
+          <Row>
+            <Col lg='6' md='6' className='m-auto text-center'>
+              <h2>Drop a Message</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Itaque maxime delectus quas deleniti consectetur esse culpa
+                eaque officiis cum ipsam?
+              </p>
+              <div className='contact mt-4'>
+                <form onSubmit={handleSubmit}>
+                  <div className='form__input'>
+                    <input
+                      type='text'
+                      placeholder='Enter your name'
+                      ref={nameRef}
+                    />
+                  </div>
+                  <div className='form__input'>
+                    <input
+                      type='email'
+                      placeholder='Enter your email'
+                      ref={emailRef}
+                    />
+                  </div>
+                  <div className='form__input'>
+                    <input
+                      type='text'
+                      placeholder='Enter subject'
+                      ref={subjectRef}
+                    />
+                  </div>
+                  <div className='form__input'>
+                    <textarea
+                      rows='10'
+                      placeholder='Write message'
+                      ref={messageRef}
+                    ></textarea>
+                  </div>
+                  <button
+                    className='send__btn '
+                    style={{
+                      border: "none",
+                      padding: "7px 25px",
+                      borderRadius: "5px",
+                      marginTop: "20px",
+                    }}
+                  >
+                    Send a Message
+                  </button>
+                </form>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      ;
+    </>
+  );
 };
 
 export default Contact;
--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -1,7 +1,67 @@
 import React from "react";
 
+import CommonSection from "../components/ui/Common-section/CommonSection";
+
+import { Container, Row, Col } from "reactstrap";
+
+import "../styles/wallet.css";
+
 const Wallet = () => {
-  return <div>Wallet</div>;
+  const wallet__data = [
+    {
+      title: "Bitcoin",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, aliquam id. Qui incidunt ad tempore nostrum quam accusantium esse aliquid.",
+      icon: "ri-bit-coin-line",
+    },
+    {
+      title: "Coinbase",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, aliquam id. Qui incidunt ad tempore nostrum quam accusantium esse aliquid.",
+      icon: "ri-coin-line",
+    },
+    {
+      title: "Metamask",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, aliquam id. Qui incidunt ad tempore nostrum quam accusantium esse aliquid.",
+      icon: "ri-copper-coin-line",
+    },
+    {
+      title: "Authereum",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, aliquam id. Qui incidunt ad tempore nostrum quam accusantium esse aliquid.",
+      icon: "ri-money-cny-circle-line",
+    },
+  ];
+  return (
+    <>
+      <CommonSection title='Wallet' />
+      <section>
+        <Container>
+          <Row>
+            <Col lg='12' className='mb-5 text-center'>
+              <div className='w-50 m-auto'>
+                <h3 className='text-light'>Connect your wallet</h3>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Maiores, aliquam id. Qui incidunt ad tempore nostrum quam
+                  accusantium esse aliquid.
+                </p>
+              </div>
+            </Col>
+
+            {wallet__data.map((item, index) => (
+              <Col lg='3' className='mb-4' key={index}>
+                <div className='wallet__item'>
+                  <span>
+                    <i className={item.icon}></i>
+                  </span>
+                  <h5>{item.title}</h5>
+                  <p>{item.desc}</p>
+                </div>
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </section>
+    </>
+  );
 };
 
 export default Wallet;
